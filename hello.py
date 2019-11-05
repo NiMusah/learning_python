fc737f739a7672e3a23c39d1ed83cc9483c686e2
musah <musa.masibo@gmail.com>
2019-11-05 14:44:02 +0300

    This is my first commit

---
+++ hello.py
@@ -0,0 +1,33 @@
+print('I am the best vesrion of my self')
+
+print(60000*12)
+
+print('We ni kubafu!!!!!!!!!!!!!!')
+
+
+#If statements
+
+number=5
+if number ==5:
+    print('Number is equals to 5')
+else:
+    print('Number not 5')
+
+#For loop
+words=['cat', 'window', 'Seth', 'Python']
+for w in words:
+    print(w, len(w))
+
+#Range function
+for i in range(5):
+    print(i)
+
+
+#My range example
+anumber = range(5)
+print(anumber)
+
+
+family=['Moses', 'Bellah', 'Seth', 'Mercy', 'Goldah', 'Mum']
+for i in range (len(family)):
+    print(i, family[i])
