917839adca3ae4ae5a43028493e4895ae4fabac0
musah <musa.masibo@gmail.com>
2019-11-06 08:02:29 +0300

    Assignment1

---
+++ assignment1.05.11.2019.py
@@ -0,0 +1,15 @@
+#Assignment 1: A function that takes 3 required parameters and returns a value
+
+#This functions attempts to calculate somebody's BMI using the Metric Method
+#Height in meters and weight in kilograms
+
+def bmicalculator(name, height, weight,):
+
+    return name, weight/(height**2)
+
+print(bmicalculator('Peter', 1.8, 150))
+
+
+
+
+
