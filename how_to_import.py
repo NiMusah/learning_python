2d25365e5a6b266ad9e5bd4b5419f862d3a1b075
musah <musa.masibo@gmail.com>
2019-11-07 11:12:25 +0300

    How To Import Modules Assignment- 7th, November 2019

---
+++ how_to_import.py
@@ -0,0 +1,20 @@
+#import math module
+
+import math
+
+    #mathematical functions
+    #pow()
+
+print(math.pow(30,2))
+
+    #pi
+
+print(math.pi)
+
+#import os module
+
+import os
+
+files= os.listdir()
+for file in files:
+    print(file)
